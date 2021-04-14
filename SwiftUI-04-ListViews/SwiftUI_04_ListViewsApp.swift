13c1145dae9d29534b7a8ead9ec307ede085721c
Mike Panitz <michpa@hotmail.com>
2021-04-13 22:20:11 -0700

    Initial Commit

---
+++ SwiftUI-04-ListViews/SwiftUI_04_ListViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_04_ListViewsApp.swift
+//  SwiftUI-04-ListViews
+//
+//  Created by Mike Panitz on 4/13/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_04_ListViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
