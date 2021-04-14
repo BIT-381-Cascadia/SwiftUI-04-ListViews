a925773599989f7ba7f0237879beabdecfab6fc1
mfear-0 <fearkenz@gmail.com>
2021-04-14 15:14:28 -0700

    Created Dynamic list with icons and buttons, ideal for future app use. Commented out a large portion of swift files to keep functionality on dynamic list. Please don't bother looking at anything else.

--- SwiftUI-04-ListViews/MultipleSelection.swift
+++ SwiftUI-04-ListViews/MultipleSelection.swift
@@ -1,53 +1,53 @@
+////
+////  MultipleSelection.swift
+////  SwiftUI-04-ListViews
+////
+////  Created by Mike Panitz on 4/14/21.
+////
 //
-//  MultipleSelection.swift
-//  SwiftUI-04-ListViews
+//import SwiftUI
 //
-//  Created by Mike Panitz on 4/14/21.
+//struct MultipleSelection: View {
+//    @State private var multiSelection = Set<UUID>()
 //
-
-import SwiftUI
-
-struct MultipleSelection: View {
-    @State private var multiSelection = Set<UUID>()
-
-    let courseList = [
-        CascadiaCourse(title: "Intro to Programming"),
-        CascadiaCourse(title: "Intermediate Programming"),
-        CascadiaCourse(title: "Advanced Programming")
-    ]
-
-    var body: some View {
-        VStack {
-            NavigationView {
-                List(selection: $multiSelection) {
-                    ForEach(courseList, id: \CascadiaCourse.id) { course in
-                        HStack {
-                            Image(systemName: "square.and.pencil").font(Font.system(.largeTitle))
-                            Text(course.title)
-                        }
-                    }
-                }
-                .navigationTitle("List Selection")
-                .toolbar {
-                    EditButton()
-                }
-            }
-            
-            if multiSelection.count == 0 {
-                Text("You haven't selected anything")
-            } else {
-                List {
-                    ForEach(Array(multiSelection), id:\.self) { selected in
-                        Text("You selected: \(selected)")
-                    }
-                }
-            }
-        }
-    }
-}
-
-struct MultipleSelection_Previews: PreviewProvider {
-    static var previews: some View {
-        MultipleSelection()
-    }
-}
+//    let courseList = [
+//        CascadiaCourse(title: "Intro to Programming"),
+//        CascadiaCourse(title: "Intermediate Programming"),
+//        CascadiaCourse(title: "Advanced Programming")
+//    ]
+//
+//    var body: some View {
+//        VStack {
+//            NavigationView {
+//                List(selection: $multiSelection) {
+//                    ForEach(courseList, id: \CascadiaCourse.id) { course in
+//                        HStack {
+//                            Image(systemName: "square.and.pencil").font(Font.system(.largeTitle))
+//                            Text(course.title)
+//                        }
+//                    }
+//                }
+//                .navigationTitle("List Selection")
+//                .toolbar {
+//                    EditButton()
+//                }
+//            }
+//            
+//            if multiSelection.count == 0 {
+//                Text("You haven't selected anything")
+//            } else {
+//                List {
+//                    ForEach(Array(multiSelection), id:\.self) { selected in
+//                        Text("You selected: \(selected)")
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//struct MultipleSelection_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MultipleSelection()
+//    }
+//}
--- SwiftUI-04-ListViews/StyledLists.swift
+++ SwiftUI-04-ListViews/StyledLists.swift
@@ -1,56 +1,56 @@
+////
+////  StyledLists.swift
+////  SwiftUI-04-ListViews
+////
+////  Created by Mike Panitz on 4/13/21.
+////
 //
-//  StyledLists.swift
-//  SwiftUI-04-ListViews
+//import SwiftUI
 //
-//  Created by Mike Panitz on 4/13/21.
+//struct StyledLists: View {
+//    var body: some View {
+//        NavigationView {
+//            VStack {
+//            NavigationLink(
+//                destination: GroupedList().listStyle(PlainListStyle()).navigationBarTitle("PlainListStyle", displayMode: .inline) ,
+//                label: {
+//                    Text("PlainListStyle:").padding()
+//                })
+//                
+//                NavigationLink(
+//                    destination: GroupedList().listStyle(InsetListStyle()).navigationBarTitle("InsetListStyle", displayMode: .inline) ,
+//                    label: {
+//                        Text("InsetListStyle:").padding()
+//                    })
+//                
+//                NavigationLink(
+//                    destination: GroupedList().listStyle(InsetGroupedListStyle()).navigationBarTitle("InsetGroupedListStyle", displayMode: .inline) ,
+//                    label: {
+//                        Text("InsetGroupedListStyle:").padding()
+//                    })
+//                
+//                NavigationLink(
+//                    destination: GroupedList().listStyle(SidebarListStyle()).navigationBarTitle("SidebarListStyle", displayMode: .inline) ,
+//                    label: {
+//                        Text("SidebarListStyle:").padding()
+//                    })
+//            }
+//            
+//            // from https://developer.apple.com/documentation/swiftui/liststyle#relationships:
+////            CarouselListStyle
+////            DefaultListStyle
+////            EllipticalListStyle
+////            GroupedListStyle
+////            InsetGroupedListStyle
+////            InsetListStyle
+////            PlainListStyle
+////            SidebarListStyle
+//        }
+//    }
+//}
 //
-
-import SwiftUI
-
-struct StyledLists: View {
-    var body: some View {
-        NavigationView {
-            VStack {
-            NavigationLink(
-                destination: GroupedList().listStyle(PlainListStyle()).navigationBarTitle("PlainListStyle", displayMode: .inline) ,
-                label: {
-                    Text("PlainListStyle:").padding()
-                })
-                
-                NavigationLink(
-                    destination: GroupedList().listStyle(InsetListStyle()).navigationBarTitle("InsetListStyle", displayMode: .inline) ,
-                    label: {
-                        Text("InsetListStyle:").padding()
-                    })
-                
-                NavigationLink(
-                    destination: GroupedList().listStyle(InsetGroupedListStyle()).navigationBarTitle("InsetGroupedListStyle", displayMode: .inline) ,
-                    label: {
-                        Text("InsetGroupedListStyle:").padding()
-                    })
-                
-                NavigationLink(
-                    destination: GroupedList().listStyle(SidebarListStyle()).navigationBarTitle("SidebarListStyle", displayMode: .inline) ,
-                    label: {
-                        Text("SidebarListStyle:").padding()
-                    })
-            }
-            
-            // from https://developer.apple.com/documentation/swiftui/liststyle#relationships:
-//            CarouselListStyle
-//            DefaultListStyle
-//            EllipticalListStyle
-//            GroupedListStyle
-//            InsetGroupedListStyle
-//            InsetListStyle
-//            PlainListStyle
-//            SidebarListStyle
-        }
-    }
-}
-
-struct StyledLists_Previews: PreviewProvider {
-    static var previews: some View {
-        StyledLists()
-    }
-}
+//struct StyledLists_Previews: PreviewProvider {
+//    static var previews: some View {
+//        StyledLists()
+//    }
+//}
